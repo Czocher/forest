1ca51582d7c0ecc207754af8112660232fd28540
Paweł Jan Czochański <czochanski@gmail.com>
2014-08-30 15:34:05 +0200

    Simple map loading.

--- src/main/java/org/czocher/forest/Example.java
+++ src/main/java/org/czocher/forest/Example.java
@@ -2,20 +2,34 @@ package org.czocher.forest;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class Example implements ApplicationListener {
+public class Example implements ApplicationListener, InputProcessor {
 
-	Texture texture;
-	SpriteBatch batch;
-	float elapsed;
+	Texture img;
+	TiledMap tiledMap;
+	OrthographicCamera camera;
+	TiledMapRenderer tiledMapRenderer;
 
 	@Override
 	public void create() {
-		texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
-		batch = new SpriteBatch();
+		final float w = Gdx.graphics.getWidth();
+		final float h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w, h);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("map.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -24,13 +38,12 @@ public class Example implements ApplicationListener {
 
 	@Override
 	public void render() {
-		elapsed += Gdx.graphics.getDeltaTime();
 		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(texture, 100 + 100 * (float) Math.cos(elapsed),
-				100 + 25 * (float) Math.sin(elapsed));
-		batch.end();
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 	}
 
 	@Override
@@ -43,6 +56,67 @@ public class Example implements ApplicationListener {
 
 	@Override
 	public void dispose() {
+	}
+
+	@Override
+	public boolean keyDown(final int keycode) {
+		if (keycode == Input.Keys.LEFT) {
+			camera.translate(-32, 0);
+		}
+		if (keycode == Input.Keys.RIGHT) {
+			camera.translate(32, 0);
+		}
+		if (keycode == Input.Keys.UP) {
+			camera.translate(0, 32);
+		}
+		if (keycode == Input.Keys.DOWN) {
+			camera.translate(0, -32);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(final char arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(final int keycode) {
+		return false;
+
+	}
+
+	@Override
+	public boolean mouseMoved(final int arg0, final int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(final int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(final int arg0, final int arg1, final int arg2,
+			final int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(final int arg0, final int arg1, final int arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(final int arg0, final int arg1, final int arg2,
+			final int arg3) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
